85258fbfe0ae3dd22363a311c3971d7d6c0d41ae
Willie Boag <william.george.boag@gmail.com>
2014-12-18 16:02:21 -0500

    add further tokenization and cache key sanitation

--- common_lib/common_features/ark_tweet/ark_tweet.py
+++ common_lib/common_features/ark_tweet/ark_tweet.py
@@ -46,19 +46,29 @@ class ArkTweetNLP:
 
     def normalizeKey(self, tweet):
         clean = lambda txt: self.h.unescape(txt).strip()
-        tmp = tweet.decode('utf-8')
+        try:
+            tmp = tweet.decode('utf-8')
+        except UnicodeEncodeError, e:
+            # Didn't want to resort to this, but get each character one at a time
+            ctmp = []
+            for c in tweet:
+                try:
+                    c.decode('utf-8')
+                    ctmp.append(c)
+                except UnicodeEncodeError, e:
+                    continue
+            tmp = ''.join(ctmp)
         return clean(clean(tmp))
 
 
     def resolve(self, original):
 
-        #print 'resolve length: ', len(data)
+        #print 'resolve length: ', len(original)
 
         data = [self.normalizeKey(twt) for twt in set(original)]
 
         # Tag all uncached data
         uncached = [ twt for twt in data if not self.cache.has_key(twt) ]
-        #uncached = data
 
         #print uncached
         #print 'len     : ', len(uncached)
@@ -71,6 +81,7 @@ class ArkTweetNLP:
             partial = CMUTweetTagger.runtagger_parse(uncached)
             print 'partial: ', len(partial)
             for twt,tag in zip(uncached,partial):
+                print 'adding: ', twt
                 self.cache.add_map(twt, tag)
 
         # Lookup all tags
@@ -83,12 +94,72 @@ class ArkTweetNLP:
         self._toks = {}
         self._pos  = {}
         for twt,tags in zip(data,tagged):
-            self._toks[twt] = [ t[0] for t in tags ]
-            self._pos[twt]  = [ t[1] for t in tags ]
+
+            # Last step of splitting compund words
+            newToks,newTags = self.post_process_tokenize(tags)
+
+            self._toks[twt] = newToks
+            self._pos[twt]  = newTags
             #print 'tweet:    ', twt
             #print 'words:    ', self._toks[twt]
             #print 'POS:      ', self._pos[twt]
             #print
+
+        #if self._toks: exit()
+
+
+    def post_process_tokenize(self, tags):
+
+        oldToks = [ t[0] for t in tags ]
+        oldTags = [ t[1] for t in tags ]
+
+        newToks = []
+        newTags = []
+
+        p = False
+        for tok,tag in zip(oldToks,oldTags):
+
+            # Case: abbreviations
+            if tok == 'w/':
+                newToks.append('with')
+                newTags.append(tag   )
+
+            # Case: Attached-with  ex. "w/Biden"
+            elif tok[:2] == 'w/':
+                newToks.append('with' )
+                newTags.append(  'P'  )
+                newToks.append(tok[2:])
+                newTags.append(tag    )
+
+            # Case: compouund word, separated with /
+            elif ( ('/' in tok)               and 
+                   (not is_url(tok))          and
+                   (len(tok.split('/')[0])>2) and 
+                   (len(tok.split('/')[1])>2)   ):
+                subs = tok.split('/')
+                newToks.append(subs[0])
+                newTags.append(tag    )  # assume same POS
+                for subTok in subs[1:]:
+                    newToks.append('/')
+                    newTags.append(',')  # don't forget the '/' separates them
+
+                    newToks.append(subTok)
+                    newTags.append(   tag)  # assume same POS
+                #p = True
+                #print tok
+                #print newToks
+                #print
+
+            # Case: default
+            else:
+                newToks.append(tok)
+                newTags.append(tag)
+        if p:
+            print oldToks
+            print newToks
+            print
+        
+        return newToks,newTags
 
 
 
@@ -146,6 +217,28 @@ class ArkTweetNLP:
         #print '\t', feats
 
         return feats
+
+
+
+
+def is_url(word):
+
+    """
+    isUrl()
+
+    Purpose: Determine if a word is a URL
+
+    @param word.   Any word from a tweet
+    @return        A boolean indicating True is 'word' is a URL, false otherwise
+    """
+
+    if word[:7]  == 'http://': return True
+    if word[:4]  == 'www.'   : return True
+    if '.com' in word: return True
+    if '.net' in word: return True
+    if '.org' in word: return True
+
+    return False
 
 
 
