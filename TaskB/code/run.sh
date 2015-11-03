8ebf57b7f2e96ba08e118807a29e3698f20d961b
wboag <william.george.boag@gmail.com>
2015-11-02 22:52:05 -0500

    remove run.sh

--- TaskB/code/run.sh
+++
@@ -1,8 +0,0 @@
-python train.py
-
-python predict.py -i ../data/twitter-test-2015-b.txt
-
-cd /data1/wboag/twitterhawk/TwitterHawk/TaskB/data/C/official_scoring
-perl score-semeval2015-task10-subtaskB.pl ../../predictions/twitter-test-2015-b.txt
-
-#python evaluate.py -e
